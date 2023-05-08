140b79ae13a7f53ceea9faa9dba87f7bc073fa6a
JoseNiltonInf <128551729+JoseNiltonInf@users.noreply.github.com>
2023-05-08 20:27:35 -0300

    desafio01.js

--- desafio01/desafio01.js
+++ desafio01/desafio01.js
@@ -1,12 +1,13 @@
-    let est = [10,23,15,78]
+let est = [10,23,15,78]
     let vend = [3,20,10,13]
     let resul = []
-
+    
     const rest = (est, vend)=>{
-        for (let i in est){
-        resul.push(est[i]- vend[i])
-        }
-        console.log("Unidades restantes:")
-        console.log(resul)
+            for (let i in est){
+                resul.push(est[i]- vend[i])
+            }
+            console.log('Resultados restantes: ')
+            return resul
+
     }
-    rest(est,vend)
+    console.log(rest(est,vend))
